58da0eea6a8fd25f18be3d67ec3a9b319207c27d
Vinicius Oliveira <viniciusoliveira@duck.com>
2022-07-12 13:29:19 -0300

    changes on the farm

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -9,8 +9,11 @@ import (
 
 // TODO: Define the SillyNephewError type here.
 type SillyNephewError struct {
-	err  error
 	cows int
+}
+
+func (err *SillyNephewError) Error() string {
+	return fmt.Sprintf("silly nephew, there cannot be %v cows", err.cows)
 }
 
 // DivideFood computes the fodder amount per cow for the given cows.
@@ -18,18 +21,17 @@ func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
 	fodderAmount, err := weightFodder.FodderAmount()
 	if err == ErrScaleMalfunction && fodderAmount >= 0 {
 		return 2 * (fodderAmount / float64(cows)), nil
-	} else if fodderAmount < 0 && (err == ErrScaleMalfunction || err == nil) {
+	}
+	if fodderAmount < 0 && (err == ErrScaleMalfunction || err == nil) {
 		return 0, errors.New("negative fodder")
-	} else if cows == 0 {
+	}
+	if cows == 0 {
 		return 0, errors.New("division by zero")
-	} else if cows < 0 {
-		value := fmt.Sprintf("silly nephew, there cannot be %v cows", cows)
-		nephew := SillyNephewError{
-			cows: cows,
-			err:  errors.New(value),
-		}
-		return 0, nephew.err
-	} else if err != nil {
+	}
+	if cows < 0 {
+		return 0, &SillyNephewError{cows: cows}
+	}
+	if err != nil {
 		return 0, err
 	}
 	return fodderAmount / float64(cows), nil
